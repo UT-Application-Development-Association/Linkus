a9971531550d622932611d473f3ec321997223a4
Zhao Tingji <tingji.zhao@mail.utoronto.ca>
2021-11-24 10:39:44 -0500

    Finished Initializing Spring Framework

---
+++ backend/src/main/java/UTADA/Linkus/LinkusApplication.java
@@ -0,0 +1,13 @@
+package UTADA.Linkus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/UTADA/Linkus/LinkusApplicationTests.java
@@ -0,0 +1,13 @@
+package UTADA.Linkus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
