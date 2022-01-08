1cd2db644ea2927f97883508febde42f29f77e7b
GareArc <cxy329288574@gmail.com>
2022-01-07 23:34:33 -0500

    add element-plus UI library

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,31 @@
+/* eslint-disable */
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = {
   transpileDependencies: ["vuex-module-decorators"],
-  publicPath: "/"
+  publicPath: "/",
+  //css and scss
+  css:{
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/themes/index.scss";`
+      },
+      // sass: {
+      //   additionalData: `@import "~@/themes/index.scss";`
+      // }
+    }
+  },
+  //plugins
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
 };
