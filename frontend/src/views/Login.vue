0c85da6d930f97dcc76d51dfd379ceae84dd4b56
xiaoyaw2 <grace.xiao@mail.utoronto.ca>
2022-01-09 21:43:51 -0500

    add login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login">
+    <v-app>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center" dense>
+          <v-col cols="12" sm="8" md="4" lg="4">
+            <v-card elevation="0">
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Enter your email" name="email" prepend-inner-icon="mdi-email" type="email" class="rounded-0" outlined></v-text-field>
+                  <v-text-field label="Enter your password" name="password" prepend-inner-icon="mdi-lock" type="password" suffix="| Forgot?" class="rounded-0" outlined></v-text-field>
+                  <v-btn class="rounded-0" color="#000000" x-large block dark>Login</v-btn>
+                  <v-card-actions class="text--secondary">
+                    <v-checkbox color="#000000" label="Remember me"></v-checkbox>
+                    <v-spacer></v-spacer>
+                    <!-- <router-link :to="{ name: 'SignUp' }">Sign Up</router-link> -->
+                    No account? <a href="#" class="pl-2" style="color: #000000">Sign Up</a>
+                  </v-card-actions>
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="ml-6 mr-6 text-center">
+                <p>By continuing, you agree to Link Us' <a href="#" class="pl-2" style="color: #000000">Policy</a> and <a href="#" class="pl-2" style="color: #000000">Terms of Use</a></p>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
